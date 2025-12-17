8cc807c0420a5b4b3a379e432b2a659b4dcabbca
Piotr Michalski <pmichalski@griddynamics.com>
2025-12-17 11:29:44 +0100

    env variable example

--- src/test/java/org/example/TestExample.java
+++ src/test/java/org/example/TestExample.java
@@ -9,4 +9,9 @@ public class TestExample {
     public void test() {
         assertThat(2).isGreaterThan(1);
     }
+
+    @Test
+    public void checkEnvVariable() {
+        assertThat(System.getenv("ENV_NAME")).isEqualTo("ENV_VALUE");
+    }
 }
