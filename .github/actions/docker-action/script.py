c1325c09176ae36f8e47de683e325243ae946b3b
Piotr Michalski <pmichalski@griddynamics.com>
2025-12-18 11:38:01 +0100

    custom docker action

--- .github/actions/docker-action/script.py
+++ .github/actions/docker-action/script.py
@@ -1,5 +1,11 @@
+import os
+
 def run():
     print("Hello from python docker")
+    input_val = os.environ['INPUT_SIMPLE-INPUT']
+    print("Simple input val: " + input_val)
+    print(f'::set-output name=simple-output""Output val')
+
 
 if __name__ == '__main__':
     run()
