9f696ee6bd4b7ef93d9d244ea60068a30f1e092b
Piotr Michalski <pmichalski@griddynamics.com>
2025-12-18 11:10:05 +0100

    custom js actions

--- .github/actions/js-action/main.js
+++ .github/actions/js-action/main.js
@@ -5,7 +5,7 @@ const exec = require('@actions/exec')
 function run() {
     const inputVal = core.getInput('simple-input', {required: true})
 
-    exec.exec('echo " Just an example execution of echo with val <inputVal> "')
+    exec.exec(`echo " Just an example execution of echo with val ${inputVal} "`)
 
     core.notice('Just a test from JS action. Input {}' + inputVal);
 }
